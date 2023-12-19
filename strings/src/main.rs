2f73c1b49c03e8a27fcbfea641c03e5906d058d4
SergeyKramp <sergey.kramp@gmail.com>
2023-12-19 06:40:51 +0100

    Added function to write a users input to a file

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,4 +1,7 @@
 use std::io;
+use std::fs;
+use std::io::Write;
+
 
 fn get_user_input() -> String {
     let mut str_to_store = String::new();
@@ -14,8 +17,19 @@ fn get_user_input() -> String {
     }
 }
 
+fn write_to_file(file_name: &str, contents: &String) {
+    match fs::File::create(file_name) {
+        Ok(mut file) => match file.write_all(contents.as_bytes()){
+            Ok(_) => (),
+            Err(error) => println!("Could not write to file: {}", error)
+        },
+        Err(error) => println!("Could not create file: {}", error)
+    };
+}
+
 fn main() {
     println!("Please enter a valid string:");
     let input = get_user_input();
     println!("You entered the value: {}", input);
+    write_to_file("output.txt", &input)
 }
