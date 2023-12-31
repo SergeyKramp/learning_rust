f47af2a02a40658e6a530aec696f9fef846cba78
SergeyKramp <sergey.kramp@gmail.com>
2023-12-31 15:21:47 +0100

    Added a test for the function to write to a file and refactored the function to take a trait an an argument

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,16 +1,23 @@
-use std::io;
 use std::fs;
-use std::io::Write;
+use std::io;
 
 const OUTPUT_FILE_NAME: &str = "output.txt";
+
+trait Writeable {
+    fn write_all(&mut self, buffer: &[u8]) -> io::Result<()>;
+}
+
+impl Writeable for fs::File {
+    fn write_all(&mut self, buffer: &[u8]) -> io::Result<()> {
+        self.write_all(buffer)
+    }
+}
 
 fn get_user_input() -> String {
     let mut str_to_store = String::new();
 
     match io::stdin().read_line(&mut str_to_store) {
-        Ok(_) => {
-           str_to_store
-        }
+        Ok(_) => str_to_store,
         Err(err) => {
             println!("Could not read input: {}", err);
             String::new()
@@ -18,19 +25,46 @@ fn get_user_input() -> String {
     }
 }
 
-fn write_to_file(file_name: &str, contents: &String) {
-    match fs::File::create(file_name) {
-        Ok(mut file) => match file.write_all(contents.as_bytes()){
-            Ok(_) => (),
-            Err(error) => println!("Could not write to file: {}", error)
-        },
-        Err(error) => println!("Could not create file: {}", error)
-    };
+fn write_to_file(file: &mut dyn Writeable, contents: &String) {
+    file.write_all(contents.as_bytes())
+        .expect("Could not write to file");
 }
 
 fn main() {
-    println!("Please enter a valid string:");
-    let input = get_user_input();
-    println!("You entered the value: {}", input);
-    write_to_file(OUTPUT_FILE_NAME, &input)
+    let user_input = get_user_input();
+    let mut file = fs::File::create(OUTPUT_FILE_NAME).expect("Could'nt open file.");
+    write_to_file(&mut file, &user_input)
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    struct MockFile {
+        contents: Vec<u8>,
+    }
+
+    impl Writeable for MockFile {
+        fn write_all(&mut self, buffer: &[u8]) -> io::Result<()> {
+            self.contents.extend_from_slice(buffer);
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn test_write_to_file() {
+        // Given
+        let test_string = "test";
+        let contents = String::from(test_string);
+        let mut mock_file = MockFile {
+            contents: Vec::new(),
+        };
+
+        // When
+        write_to_file(&mut mock_file, &contents);
+
+        // Then
+        assert_eq!(String::from_utf8(mock_file.contents).unwrap(), contents)
+    }
 }
